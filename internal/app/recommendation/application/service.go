f85aa352f9b28005c432c2ab9dd3489028d92336
MagicalLas <haho013039@gmail.com>
2021-02-05 17:01:39 +0900

    wip

---
+++ internal/app/recommendation/application/service.go
@@ -0,0 +1,9 @@
+package application
+
+import "magical.dev/tranquility/internal/app/recommendation/domain"
+
+type MusicRecommendationService struct {}
+
+func (s *MusicRecommendationService) RecommendMusic(userID string) []domain.Music {
+	return nil
+}
